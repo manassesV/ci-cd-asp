374e4d93f5e11d37064adfe89f87ea1c5cf4c8ac
manasses <manavitorino@gmail.com>
2021-05-20 16:04:00 -0700

    mm

---
+++ Tests/ExampleTests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Net;
+using NUnit;
+using Xunit;
+
+namespace Tests
+{
+    public class ExampleTests
+    {
+        [Fact]
+        public void TestSuccess1()
+        {
+
+        }
+
+
+        [Fact]
+        public void TestSuccess2()
+        {
+
+        }
+
+        [Theory]
+        [InlineData("https://www.google.com")]
+        [InlineData("fffaaa")]
+        public void TestMalformedUri(string uri)
+        {
+            new Uri(uri);
+        }
+    }
+}
